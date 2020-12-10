98e5b366d42067f03fc590dcfc0b1a7ba9cb8b8e
Jonah Keller <jkeller10@stuy.edu>
2020-12-10 12:06:44 -0500

    Tried to confirm if my method signatures were correct.

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,5 +2,22 @@ import java.util.ArrayList;
 
 public class NoNullArrayList<T> extends ArrayList<T>
 {
+  private T value;
+
+  public boolean add()
+  {
+
+  }
+
+  public boolean add(int index)
+  {
+
+  }
+
+  public value set(index, value)
+  {
+
+  }
+
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # OrderedArrayList
 We talked about the file name briefly.
 We talked about whether we need to include the generic or not in the file name
+went over the correct method signatures
